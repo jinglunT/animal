c3bab3ef19bfa20ca5526e24cf2550d02de3fb0e
Bestian Tang <bestian@gmail.com>
2021-05-22 09:22:43 +0800

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,6 +3,6 @@
     <h1>影音</h1>
     <iframe width="700" height="394" src="https://www.youtube.com/embed/EPFp3ihdmn0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     <iframe width="700" height="394" src="https://www.youtube.com/embed/CodcXeL75c0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    <iframe width="1280" height="720" src="https://www.youtube.com/embed/O4-5gxL6kLg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe width="700" height="394" src="https://www.youtube.com/embed/O4-5gxL6kLg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   </div>
 </template>
