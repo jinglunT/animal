9f811c1d31395857d037bfe13d9befe399278461
jinglunT <61607618+jinglunT@users.noreply.github.com>
2021-05-22 10:01:21 +0800

    20210522

--- src/components/Reptiles.vue
+++ src/components/Reptiles.vue
@@ -1,9 +1,29 @@
 <template>
   <div class="hello">
   <vue-headful title="首頁"/>
+   <div class="ui grid container">
+      <div class="ui row">
+        <div class="column">
+          <img class = "small" :src="'./IMG_4451.JPG'"/>
+        </div>
+        <div class="ten wide column">
+          <router-link to ="/reptiles2" target="_blank">豹紋守宮</router-link>
+        </div>
+      </div>
+      <div class="ui row">
+        <div class="column">
+          <img class = "small" :src="'./IMG_4451.JPG'"/>
+        </div>
+        <div class="ten wide column">
+          <router-link to ="/reptiles3" target="_blank">肥尾守宮</router-link>
+        </div>
+      </div>
+      <div class="ui row">
   <img :src="'/IMG_4451.jpg'">
   <p>睫角守宮</p>
-  </div>
+      </div>
+    </div>
+  </div>  
 </template>
 
 <script>
