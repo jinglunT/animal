c486c0e47bee0a27174f79e2e4c955e6c9f5802e
Bestian Tang <bestian@gmail.com>
2021-07-03 09:23:19 +0800

    add loadash, vuemarkdown

--- src/main.js
+++ src/main.js
@@ -3,12 +3,20 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import { firestorePlugin } from 'vuefire'
+import VueLodash from 'vue-lodash'
+import lodash from 'lodash'
+import VueMarkdown from 'vue-markdown'
+
+Vue.use(VueLodash, { name: 'custom' , lodash: lodash })
 
 Vue.use(firestorePlugin)
 
 Vue.config.productionTip = false
 
 new Vue({
+  components: {
+    VueMarkdown
+  },
   router,
   render: h => h(App)
 }).$mount('#app')
