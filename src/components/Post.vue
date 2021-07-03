e76b7564acefe9e3def3358e3c37374a248e0d3c
jinglunT <61607618+jinglunT@users.noreply.github.com>
2021-07-03 09:22:38 +0800

    post

---
+++ src/components/Post.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+  <img :src="'./首頁背景.JPG'">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+img {
+  width:80%
+}
+a {
+  color: #42b983;
+}
+</style>
