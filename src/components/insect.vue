5bec8885b527530dbe6b0cf51a46c9ed0908ccfe
Bestian Tang <bestian@gmail.com>
2021-07-10 10:01:58 +0800

    post

--- src/components/insect.vue
+++ src/components/insect.vue
@@ -4,9 +4,7 @@
      <div class="ui grid container">
       <div v-for="(p,idx) in post" class="ui row" :key="idx" :class="{hidden: p.type != '昆蟲與節肢動物類' || !p.img }" >
         <router-link :to = "'/p/' + idx">
-          
-          <img class = "tiny" :src="p.img"/>
-          <div>{{p.user}}說:{{p.text.substr(0,30)}}...</div>
+          <div><img class = "tiny" :src="p.img"/>{{p.user}}說:{{p.text.substr(0,30)}}...</div>
         </router-link>
       </div>
       <div class="ui row">
