279b8389564f1defc43a020ee94b656562ca8a7e
jinglunT <61607618+jinglunT@users.noreply.github.com>
2021-08-14 10:11:33 +0800

    post img

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -8,6 +8,7 @@
       <li v-show="!more"><button @click="more = true">看更多</button></li>
       <li v-show="more">如果有推薦的飼料或用品或店家，都是可以分享出來的，不過請不要變成打廣告。</li>
       <li v-show="more">如要文內附圖，請用markdown語法：![替代文字](圖片網址)</li>
+      <li v-show="more"><img src="螢幕擷取畫面 (389).jpg"></li>
     </ol>
     <form>
       <input type="text" name="" v-model="name" placeholder="您的大名" />
