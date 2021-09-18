8d35905a571ec3b6276a52c98343acdb989124c0
Bestian Tang <bestian@gmail.com>
2021-09-18 10:02:53 +0800

    deploy

---
+++ precache-manifest.91b0d7e4ac58529122fd1202491610ae.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6edac1a1faf4115b9a9c159eb0df106",
+    "url": "/animal/DSC09821.JPG"
+  },
+  {
+    "revision": "aa5c17bd111b56957d4b0087335db62e",
+    "url": "/animal/IMG_3254.JPG"
+  },
+  {
+    "revision": "be8f7db475919be5c25e1b4192b6dee3",
+    "url": "/animal/IMG_3259.JPG"
+  },
+  {
+    "revision": "1d6016d12fc5d6776a4492fc358c8d1d",
+    "url": "/animal/IMG_5220.JPG"
+  },
+  {
+    "revision": "924f0fefb1c15d5a69c5947b7d5de384",
+    "url": "/animal/IMG_5390.JPG"
+  },
+  {
+    "revision": "a7cfe98c15b9c7d3d0436faaf63ff8d5",
+    "url": "/animal/IMG_7680.JPG"
+  },
+  {
+    "revision": "abf8ae72938758788bb28179adc73024",
+    "url": "/animal/IMG_7686.JPG"
+  },
+  {
+    "revision": "288ae24a6114f39ce0b7a23d9816c135",
+    "url": "/animal/IMG_7689.JPG"
+  },
+  {
+    "revision": "04fc759daa9789e8c9eea358790d43d2",
+    "url": "/animal/IMG_7715.JPG"
+  },
+  {
+    "revision": "d56f22ea9731b47153ffb721bc759cc1",
+    "url": "/animal/IMG_7719.JPG"
+  },
+  {
+    "revision": "a8ccefff6067a178b70d5a40f0c881fc",
+    "url": "/animal/IMG_8192.JPG"
+  },
+  {
+    "revision": "2352911eddbba276c74116b1fd137559",
+    "url": "/animal/IMG_9503.JPG"
+  },
+  {
+    "revision": "214fcd593854e3bc11b41a252f0cc2a4",
+    "url": "/animal/a.json"
+  },
+  {
+    "revision": "47f09082ea7a7e736dda",
+    "url": "/animal/css/about.8562c90b.css"
+  },
+  {
+    "revision": "a11e4b2e9dd92a49b2b8",
+    "url": "/animal/css/app.53f23d77.css"
+  },
+  {
+    "revision": "d5637a610b8245e872cf274a07b0588a",
+    "url": "/animal/i.json"
+  },
+  {
+    "revision": "5666625d58d612fe052bbfeab25a7459",
+    "url": "/animal/img_4451.jpg"
+  },
+  {
+    "revision": "95d2c8ced2cbba72019c96f8cdf8abfe",
+    "url": "/animal/index.html"
+  },
+  {
+    "revision": "47f09082ea7a7e736dda",
+    "url": "/animal/js/about.20c355cb.js"
+  },
+  {
+    "revision": "a11e4b2e9dd92a49b2b8",
+    "url": "/animal/js/app.fc8cd2e2.js"
+  },
+  {
+    "revision": "71c6384e38362605352c",
+    "url": "/animal/js/chunk-vendors.46164f4d.js"
+  },
+  {
+    "revision": "f6cbd24a22a3b5f5bfabb25dcdbead98",
+    "url": "/animal/manifest.json"
+  },
+  {
+    "revision": "9f571f584f863ac552c39c464f1c4018",
+    "url": "/animal/r.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/animal/robots.txt"
+  },
+  {
+    "revision": "1388c9868681677d2ad6bb6fa91c7948",
+    "url": "/animal/螢幕擷取畫面 (360).png"
+  },
+  {
+    "revision": "5875a6c94764ba58fe38d2dc006249e6",
+    "url": "/animal/螢幕擷取畫面 (362).png"
+  },
+  {
+    "revision": "1d5e94677f9e3aeabf3704c41a419b15",
+    "url": "/animal/螢幕擷取畫面 (363).png"
+  },
+  {
+    "revision": "e8b7f7b8ab3b406f9156d2ebbdd8583f",
+    "url": "/animal/螢幕擷取畫面 (389).jpg"
+  },
+  {
+    "revision": "f23dacf960abcbb310f6a383d1d4b262",
+    "url": "/animal/首頁背景.JPG"
+  },
+  {
+    "revision": "065fd78ff469371c028b73d0c6ad0bb0",
+    "url": "/animal/首頁背景1.JPG"
+  }
+]);
